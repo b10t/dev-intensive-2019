8a44d60c4dead8828e28f9befc41a0262ebe095b
b10t <b10t.rus@gmail.com>
2019-07-17 16:58:10 +0800

    27:05

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,74 @@
 package ru.skillbranch.devintensive
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
 class MainActivity : AppCompatActivity() {
+    // lateinit - обязательно инициализируем позже
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBt: ImageView
+
+    lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("M_MainActivity", "onCreate")
+
+//        benderImage = findViewById<ImageView>(R.id.iv_bender)
+
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBt = iv_send
+
+        benderObj = Bender()
+
+        textTxt. text = benderObj.askQuestion()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d("M_MainActivity", "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("M_MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("M_MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("M_MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("M_MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("M_MainActivity", "onDestroy")
     }
 }
