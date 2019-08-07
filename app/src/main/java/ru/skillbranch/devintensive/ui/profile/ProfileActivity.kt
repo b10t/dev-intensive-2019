ab2c8ba9fb08f478ce441c4f4671d057e1a3ba5c
b10t <b10t.rus@gmail.com>
2019-08-07 16:43:37 +0800

    48:40

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,20 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.skillbranch.devintensive.R
+
+class ProfileActivity : AppCompatActivity() {
+    // lateinit - обязательно инициализируем позже
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+    }
+}
